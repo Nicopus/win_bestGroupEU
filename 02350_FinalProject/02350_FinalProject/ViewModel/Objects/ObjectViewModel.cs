79e95be7038b363f24a1b5c9035bd845aaf38818
Nicolai Hansen <nicolaihansen@live.dk>
2016-10-25 18:04:50 +0200

    Mere model

---
+++ 02350_FinalProject/02350_FinalProject/ViewModel/Objects/ObjectViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02350_FinalProject.Model;
+
+namespace _02350_FinalProject.ViewModel.Objects
+{
+    public abstract class ObjectViewModel : BaseViewModel, IObject
+    {
+
+        #region Object Wrapper
+        protected Model.Object Object { get; }
+        protected ObjectViewModel(Model.Object obj)
+        {
+            Object = obj;
+        }
+        public List<string> Data { get; set; }
+
+        public double Height
+        {
+            get { return Object.Height; }
+            set
+            {
+                Object.Height = value;
+            }
+        }
+
+        public int Number => Object.Number;
+
+        public EObject Type => Object.Type;
+
+        public double Width
+        {
+            get { return Object.Width; }
+            set
+            {
+                Object.Width = value;
+            }
+        }
+
+        public double X
+        {
+            get { return Object.X; }
+            set
+            {
+                Object.X = value;
+            }
+        }
+
+        public double Y
+        {
+            get { return Object.Y; }
+            set
+            {
+                Object.Y = value;
+            }
+        }
+    }
+
+    #endregion
+}
